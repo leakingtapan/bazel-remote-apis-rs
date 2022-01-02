ed9aa0d552f8ea40d7901c30fca8a4f618ebe314
Alex <pancheng1987@gmail.com>
2022-01-01 22:26:22 -0800

    Build and generate build.bazel.remote.execution.v2.rs

---
+++ build.rs
@@ -0,0 +1,11 @@
+use tonic_build;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(false)
+        .compile(
+           &["proto/bazel-remote-apis/build/bazel/remote/execution/v2/remote_execution.proto"],
+           &["proto/bazel-remote-apis", "proto/googleapis"],
+        )?;
+    Ok(())
+}
