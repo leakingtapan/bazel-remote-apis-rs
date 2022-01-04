77482ef9dfe6f924f9774be45ab8cee111b79aee
Cheng Pan <pancheng1987@gmail.com>
2022-01-04 00:06:18 -0800

    add generated code as module

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,24 @@
+pub mod build {
+    pub mod bazel {
+        pub mod remote {
+            pub mod execution {
+                pub mod v2 {
+                    tonic::include_proto!("build.bazel.remote.execution.v2");
+                }
+            }
+        }
 
+        pub mod semver {
+            tonic::include_proto!("build.bazel.semver");
+        }
+    }
+}
+
+pub mod google {
+    pub mod rpc {
+        tonic::include_proto!("google.rpc");
+    }
+    pub mod longrunning {
+        tonic::include_proto!("google.longrunning");
+    }
+}
