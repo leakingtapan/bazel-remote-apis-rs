eb7f8f5cca0cdea36b4053daf5eb25c43ca1c41c
Cheng Pan <pancheng1987@gmail.com>
2022-01-10 00:35:14 -0800

    build bytestream proto as well

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
 use tonic_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(
-           &["proto/bazel-remote-apis/build/bazel/remote/execution/v2/remote_execution.proto"],
-           &["proto/bazel-remote-apis", "proto/googleapis"],
-        )?;
+    tonic_build::configure().build_server(false).compile(
+        &[
+            "proto/bazel-remote-apis/build/bazel/remote/execution/v2/remote_execution.proto",
+            "proto/googleapis/google/bytestream/bytestream.proto",
+        ],
+        &["proto/bazel-remote-apis", "proto/googleapis"],
+    )?;
     Ok(())
 }
